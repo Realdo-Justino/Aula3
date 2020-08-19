68a9170e7b4b55b262287a8e0f998cd13bfc92fd
unknown <realdojj@gmail.com>
2020-08-19 17:21:23 -0300

    teste

---
+++ Aula-03.py
@@ -0,0 +1,11 @@
+print("hello world")
+nome1="Realdo"
+nome2="Justino"
+print(nome1)
+print(type(nome1))
+print(nome1*3)
+print(nome1+'', nome2)
+#Anotações
+voar=2.44400000000000012
+print(round(voar,2))
+print("%.2f" %(voar))
